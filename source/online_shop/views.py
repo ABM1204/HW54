5638981fcf0df2c5d10da21d87037436f4232a70
ABM1204 <bekzat.melisov@iaau.edu.kg>
2024-07-01 16:44:20 +0600

    Добвил функции добавления категорий и товара

--- source/online_shop/views.py
+++ source/online_shop/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponseNotFound
-from django.shortcuts import render, get_object_or_404
-from online_shop.models import Product
-
+from django.http import HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404, redirect
+from online_shop.models import Product, Category
 
 def index(request):
     products = Product.objects.all()
@@ -11,5 +10,45 @@ def product_view(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, 'product_view.html', {'product': product})
 
+def category_add_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
 
+        if not name:
+            return HttpResponseBadRequest('Name cannot be empty')
 
+        category = Category(name=name, description=description)
+        category.save()
+        return redirect('products_view')
+
+    return render(request, 'add_category.html')
+
+def product_add_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        category_id = request.POST.get('category')
+        price = request.POST.get('price')
+        image_url = request.POST.get('image_url')
+
+        if not name:
+            return HttpResponseBadRequest('Name cannot be empty')
+
+        if not category_id:
+            return HttpResponseBadRequest('Category cannot be empty')
+
+        if not price:
+            return HttpResponseBadRequest('Price cannot be empty')
+
+        if not image_url:
+            return HttpResponseBadRequest('Image URL cannot be empty')
+
+        category = get_object_or_404(Category, id=category_id)
+
+        product = Product(name=name, description=description, category=category, price=price, image=image_url)
+        product.save()
+        return redirect('products_view')
+
+    categories = Category.objects.all()
+    return render(request, 'add_product.html', {'categories': categories})
